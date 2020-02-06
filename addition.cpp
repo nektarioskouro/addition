29bac1eabb7d52ccda8d70331cb98ae3599df3e6
nektarioskouro <54715648+nektarioskouro@users.noreply.github.com>
2020-02-06 05:35:17 -0500

    Add files via upload

---
+++ addition.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x, y;
+    int sum;
+    cout << "First number: ";
+    cin >> x;
+    cout << "Second number: ";
+    cin >> y;
+    sum = x + y;
+    cout << "Result: " << sum;
+    return 0;
+}
